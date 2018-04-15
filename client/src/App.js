503ddc10e45591530109a4f9afeb3cd7ac5e05c4
stedyyulius <stedyyulius@gmail.com>
2018-04-15 15:10:37 +0700

    geocoder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import GoogleMapReact from 'google-map-react';
+import geocoder from 'geocoder'
 
 import './App.css';
 
@@ -12,10 +13,8 @@ class App extends Component {
     super(props)
     this.state={
       news: [],
-      center: {
-        lat: -6.1751,
-        lng: 106.8650
-      },
+      lat: -6.1751,
+      lng: 106.8650,
       zoom: 5,
       city: []
     }
@@ -34,38 +33,33 @@ class App extends Component {
         city: cities.data
       })
     })
-
   }
+
+getLocation(loc){
+ geocoder.geocode(loc,(err,data)=>{
+   if(typeof data !== "undefined" && data.status === 'OK'){
+     this.setState({
+       lat: data.results[0].geometry.location.lat,
+       lng: data.results[0].geometry.location.lng
+     })
+    }
+  })
+ }
 
   render() {
     return (
       <div className="App">
+      <input type="text" onChange={(e) => this.getLocation(e.target.value)} />
+      <br /><br /><br />
       <div className="map">
         <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyDiA5cTYkoL3zlGqYLBN6i4txZ9IsWRpl4'}}
-        defaultCenter={this.state.center}
+        bootstrapURLKeys={{ key: 'AIzaSyBkfI-yQQofBFbKd7SwESDh-go2PzFP0Cw'}}
+        center={{lat: this.state.lat, lng: this.state.lng}}
         defaultZoom={this.state.zoom}
       >
-      {this.state.city.map((c, i)=>
-        <div key={i}
-        lat={c.latlng[0]}
-        lng={c.latlng[1]}>
-        <h6>{c.name}</h6>
-          <img
-          className="awan"
-          src="http://www.luxus-india.com/wp-content/uploads/2015/12/stock-vector-flat-city-icon-209365492.png"
-          />
-        </div>
-      )}
+
         </GoogleMapReact>
       </div>
-        {this.state.news.map((n, i)=>
-          <div key={i}>
-            <h6>{n.title}</h6>
-            <a href={n.url}>{n.url}</a>
-            <img src={n.urlToImage} />
-          </div>
-        )}
       </div>
     );
   }
